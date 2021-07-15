a9f4e3285d483ff0507d1ca16b2bdbcf37d5eb14
Nam Nguyen <namvannguyen.andy@gmail.com>
2021-07-15 21:55:49 +0700

    add menu bar

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
+import { Menu } from 'antd';
+import * as AppRoutes from '../../router/router';
 
 import { getDeviceDetail } from '../../redux/actions/homeAction';
 
@@ -9,6 +12,7 @@ const HomePage = ({ handleGetDeviceDetail, homeReducer }) => {
   const { images } = homeReducer;
   const [img1, setImg1] = useState('/assets/cat.jpg');
   const [img2, setImg2] = useState('/assets/cat.jpg');
+  const [currentLink, setCurrentLink] = useState('Home');
   useEffect(() => {
     handleGetDeviceDetail();
   }, []);
@@ -50,36 +54,74 @@ const HomePage = ({ handleGetDeviceDetail, homeReducer }) => {
     console.log('kidding me');
   };
 
+  const handleClick = (data) => {
+    setCurrentLink(data.key);
+  };
+
   return (
-    <div className="d-flex align-items-center justify-content-around height-100-per widht-100-per">
-      <div className="d-flex width-28-per max-height-by-px-500">
-        <img className="width-100-per" src={img1} alt=" not found" />
-      </div>
-      <div className="width-10-per height-100-per min-width-by-px-90 justify-content-center d-flex flex-column">
-        <Button
-          onMouseDown={handleSwitch}
-          onMouseUp={handleRelease}
-          type="button"
-          className="m-b-20"
-          color="primary"
-        >
-          Switch
-        </Button>
-        <Button onClick={approve} type="button" className="m-b-20" color="success">
-          Approved
-        </Button>
-        <Button onClick={clearResult} type="button" className="m-b-20" color="info">
-          Clear Result
-        </Button>
-        <Button onClick={prev} type="button" className="m-b-20" color="danger">
-          Prev
-        </Button>
-        <Button onClick={next} type="button" className="m-b-20" color="primary">
-          Next
-        </Button>
-      </div>
-      <div className="d-flex width-28-per max-height-by-px-500">
-        <img className="width-100-per" src={img2} alt=" not found" />
+    <div className="width-100-per">
+      <Menu onClick={handleClick} selectedKeys={[currentLink]} mode="horizontal">
+        <Menu.Item key="Home">Files</Menu.Item>
+        <Menu.Item key="Edit">Edit</Menu.Item>
+        <Menu.Item key="View">View</Menu.Item>
+        <Menu.Item key="Help">
+          <NavLink
+            to={AppRoutes.HOME}
+          >
+            Help
+          </NavLink>
+        </Menu.Item>
+      </Menu>
+      <div className="d-flex align-items-center justify-content-around height-100-per widht-100-per">
+        <div className="d-flex width-28-per max-height-by-px-500">
+          <img className="width-100-per" src={img1} alt=" not found" />
+        </div>
+        <div className="width-10-per height-100-per min-width-by-px-90 justify-content-center d-flex flex-column">
+          <Button
+            onMouseDown={handleSwitch}
+            onMouseUp={handleRelease}
+            type="button"
+            className="m-b-20"
+            color="primary"
+          >
+            Switch
+          </Button>
+          <Button
+            onClick={approve}
+            type="button"
+            className="m-b-20"
+            color="success"
+          >
+            Approved
+          </Button>
+          <Button
+            onClick={clearResult}
+            type="button"
+            className="m-b-20"
+            color="info"
+          >
+            Clear Result
+          </Button>
+          <Button
+            onClick={prev}
+            type="button"
+            className="m-b-20"
+            color="danger"
+          >
+            Prev
+          </Button>
+          <Button
+            onClick={next}
+            type="button"
+            className="m-b-20"
+            color="primary"
+          >
+            Next
+          </Button>
+        </div>
+        <div className="d-flex width-28-per max-height-by-px-500">
+          <img className="width-100-per" src={img2} alt=" not found" />
+        </div>
       </div>
     </div>
   );
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,3 @@
 module.exports = {
-    LOGIN: '/',
-    TOPIC_LIST: '/topics',
-    HOME: '/home',
-    PROBLEM: '/problem',
-    YOUR_ACCOUNT: '/youraccount',
-    LOGOUT: 'LOGOUT',
-    CLASSROOM: '/classroom',
-    CHALLENGE: '/challenge',
+    HOME: '/',
 };
